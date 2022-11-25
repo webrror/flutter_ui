069551eae8d20fcd44f8c47f10199c17192e0248
Jerin <jerinjacob0206@gmail.com>
2022-11-25 12:27:33 +0530

    (WIP) Alternate Hotel UI

---
+++ lib/Hotel/Alternate Hotel UI/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Hotel/Alternate%20Hotel%20UI/screens/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(useMaterial3: true),
+    debugShowCheckedModeBanner: false,
+    home: const Home(),
+  ));
+}
